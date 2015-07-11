ce606a868e1034cadd6575cec0485997024acca9
jorgipor <elgorje@gmail.com>
2015-07-11 19:34:02 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,34 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', {pregunta: quiz[0].pregunta, title: 'Quiz'})
-	})
+// GET /quizes
+// GET /users/:userId/quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes});
+    })
 };
 
-// GET /quizes/answer
-exports.answer = function(req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		if(req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz'});
-		} else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Quiz'});
-		}
+// GET /quizes/:id
+exports.show = function(req, res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+  	res.render('quizes/show', { quiz: quiz});
 	})
+};           
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+  if (req.query.respuesta === quiz.respuesta) {
+    res.render('quizes/answer', 
+    { quiz: quiz, 
+      respuesta: 'Correcto'
+    });
+  } else {
+  	res.render('quizes/answer', 
+    { quiz: quiz, 
+      respuesta: 'Incorrecto'
+    });
+  }
+})
 };
