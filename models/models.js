66ab82326ff7b75a2d4bebc80cef92fd6a0d8347
jorgipor <elgorje@gmail.com>
2015-07-14 20:00:52 +0200

    Preguntas con temática

--- models/models.js
+++ models/models.js
@@ -44,11 +44,13 @@ sequelize.sync().then(function() {
           if(count === 0) {   // la tabla se inicializa solo si está vacía
              Quiz.create({
               pregunta: 'Capital de Italia',
-              respuesta: 'Roma'
+              respuesta: 'Roma',
+              tema: 'Otro'
             });
             Quiz.create({
               pregunta: 'Capital de Portugal',
-              respuesta: 'Lisboa'
+              respuesta: 'Lisboa',
+              tema: 'Otro'
             }).then(function(){console.log('Base de datos inicializada')});
           };
         });
