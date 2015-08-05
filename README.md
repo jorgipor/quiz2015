43002e8d8f3bf54bfe55eee5b2b2296e60480cbc
jorgipor <elgorje@gmail.com>
2015-08-05 22:17:06 +0200

    Autenticacion

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ https://jorgipor-quiz-2015.herokuapp.com/
 
 foreman start
 http://localhost:5000/
+
+// Ver log de commits
+git log --oneline
+
+// Añadir todo (para nuevos ficheros)
+git add .
+
+// Hacer commit
+git commit -m "Nombre del commit"
+
+// Ver rutas
+git remote -v
+
+// Subir al repositorio origin
+git push
+
+//subir a Heroku
+git push heroku master
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -28,21 +28,17 @@ exports.load = function(req, res, next, userId) {
   ).catch(function(error){next(error)});
 };
 
+var users = { admin: {id : 1, username: "admin", password: "1234"},
+				pepe: {id : 2, username: "pepe", password: "5678"}};
 // Comprueba si el usuario esta registrado en users
 // Si autenticación falla o hay errores se ejecuta callback(error).
 exports.autenticar = function(login, password, callback) {
-	models.User.find({
-        where: {
-            username: login
-        }
-    }).then(function(user) {
-    	if (user) {
-    		if(user.verifyPassword(password)){
-            	callback(null, user);
+    	if (users[login]) {
+    		if(password === users[login].password){
+            	callback(null, users[login]);
         	}
         	else { callback(new Error('Password erróneo.')); } 	
-      	} else { callback(new Error('No existe user=' + login))}
-    }).catch(function(error){callback(error)});
+      	} else { callback(new Error('No existe user=' + login));}
 };
 
 
